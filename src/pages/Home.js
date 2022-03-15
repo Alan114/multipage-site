7dd6110efe7ef048de016d8b4487bb2bef94ef02
Alan Pecenkovic <alanquad@yahoo.com>
2022-03-15 14:52:24 +0100

    Fetching articles from JSON server, syling and outputting them

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,25 @@
 import React from "react";
+import useFetch from "../hooks/useFetch";
+import "./Home.css";
 
 const Home = () => {
+  const {
+    data: articles,
+    isPending,
+    error,
+  } = useFetch("http://localhost:3000/articles");
   return (
-    <div>
-      <h2>Homepage</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
-        aspernatur eos, laboriosam in sapiente odio natus atque hic reiciendis,
-        optio accusantium itaque aliquid ipsa recusandae distinctio quas nisi,
-        placeat provident.
-      </p>
+    <div className="home">
+      <h2>Articles</h2>
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {articles &&
+        articles.map((article) => (
+          <div key={article.id} className="card">
+            <h3>{article.title}</h3>
+            <p>{article.author}</p>
+          </div>
+        ))}
     </div>
   );
 };
