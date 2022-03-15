9eaa6662ae0332949ddee4680f11644434349dd4
Alan Pecenkovic <alanquad@yahoo.com>
2022-03-15 11:56:59 +0100

    Creating page components and setting up Routes with React Router

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
+        aspernatur eos, laboriosam in sapiente odio natus atque hic reiciendis,
+        optio accusantium itaque aliquid ipsa recusandae distinctio quas nisi,
+        placeat provident.
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Homepage</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
+        aspernatur eos, laboriosam in sapiente odio natus atque hic reiciendis,
+        optio accusantium itaque aliquid ipsa recusandae distinctio quas nisi,
+        placeat provident.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
