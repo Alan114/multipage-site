caae3bcb37664f3a565563382040a248759685d4
Alan Pecenkovic <alanquad@yahoo.com>
2022-03-16 12:14:29 +0100

    Getting query parameter with useLocation().search and then using URLSearchParams to use the get method on it

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 const Contact = () => {
+  const queryString = useLocation().search;
+  console.log(queryString);
+
+  const queryParams = new URLSearchParams(queryString);
+  const name = queryParams.get("name");
+
   return (
     <div>
-      <h2>Contact</h2>
+      <h2>Hey {name}, Contact us here...</h2>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
         aspernatur eos, laboriosam in sapiente odio natus atque hic reiciendis,
